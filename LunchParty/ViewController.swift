e45d98d97709c6982b834e550d9c764c8355f67c
Melissa Ma <mm16bn@my.fsu.edu>
2020-11-09 18:26:03 -0500

    Add root view controller

---
+++ LunchParty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LunchParty
+//
+//  Created by Melissa on 11/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
